90b95752f5aabd36a199a8dfc384e8b0088b643c
andesdevroot <andes.dev.root@gmail.com>
2021-07-07 10:37:36 -0400

    se crea modelos User y Role mas se indica relaciones entre ambos modelos

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ class NameForm(FlaskForm):
     name = StringField('Cual es tu nombre?', validators=[Required()])       
     submit = SubmitField('Enviar')
 
+#pagina index
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -44,6 +45,7 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
+#pagina user
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)       
@@ -56,6 +58,27 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+
+#MODELOS DEFINICIONES ORM
+
+class Role(db.Model):       
+    __tablename__ = 'roles'       
+    id = db.Column(db.Integer, primary_key=True)       
+    name = db.Column(db.String(64), unique=True)    
+    users = db.relationship('User', backref='role', lazy='dynamic')   
+          
+    def __repr__(self):       
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 
